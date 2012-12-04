3456d2c97a09b60260adec6a835824b0dd7794c1
Neil Bowers <neil@bowers.com>
2012-12-04 12:56:26 +0000

    Added delete_bounces() method and switched to the JSON endpoint (from the XML)

--- lib/Mail/SendGrid.pm
+++ lib/Mail/SendGrid.pm
@@ -5,75 +5,136 @@ package Mail::SendGrid;
 
 use Mouse 0.94;
 use HTTP::Tiny 0.013;
-use XML::Simple 2.18;
+use JSON 2.53;
 use URI::Escape 3.30;
 use Carp 1.20;
 
 use Mail::SendGrid::Bounce;
 
-has 'api_user'     => (is => 'ro', isa => 'Str', required => 1);
-has 'api_key'      => (is => 'ro', isa => 'Str', required => 1);
+has 'api_user'  => (is => 'ro', isa => 'Str', required => 1);
+has 'api_key'   => (is => 'ro', isa => 'Str', required => 1);
+has 'ua'        => (is => 'ro', default => sub { HTTP::Tiny->new(); });
 
-my %bounce_params =
+my %valid_params =
 (
-    days       => '\d+',
-    start_date => '\d\d\d\d-\d\d-\d\d',
-    end_date   => '\d\d\d\d-\d\d-\d\d',
-    limit      => '\d+',
-    offset     => '\d+',
-    type       => 'hard|soft',
-    email      => '\S+@\S+',
+
+    'bounces.get' =>
+    {
+        days       => '\d+',
+        start_date => '\d\d\d\d-\d\d-\d\d',
+        end_date   => '\d\d\d\d-\d\d-\d\d',
+        limit      => '\d+',
+        offset     => '\d+',
+        type       => 'hard|soft',
+        email      => '\S+@\S+',
+    },
+
+    'bounces.delete' =>
+    {
+        start_date => '\d\d\d\d-\d\d-\d\d',
+        end_date   => '\d\d\d\d-\d\d-\d\d',
+        type       => 'hard|soft',
+        email      => '\S+@\S+',
+    },
+
 );
 
 sub bounces
 {
     my $self     = shift;
     my %opts     = @_;
-    my $base_uri = 'https://sendgrid.com/api/bounces.get.xml';
+    my $response;
+    my $url;
+    my $bounce_list;
+    my (@bounces, $bounce);
+
+    $response = $self->_make_request('bounces.get', \%opts, { date => 1 });
+
+    if ($response->{success}) {
+        $bounce_list = decode_json($response->{content});
+        foreach my $bounce_details (@{ $bounce_list }) {
+            $bounce = Mail::SendGrid::Bounce->new($bounce_details);
+            push(@bounces, $bounce) if defined($bounce);
+        }
+    }
+
+    return @bounces;
+}
+
+sub delete_bounces
+{
+    my $self = shift;
+    my %opts     = @_;
+    my $base_uri = 'https://sendgrid.com/api/bounces.delete.json';
+    my $response;
+    my $json;
+    my $url;
+
+    $response = $self->_build_url('bounces.delete', \%opts, {});
+
+    if ($response->{success}) {
+        $json = decode_json($response->{content});
+        if ($json->{message} eq 'success') {
+            print STDERR "bounces.delete success: response= $response->{content}\n";
+            return 1;
+        } elsif (exists($json->{message})) {
+            carp "bounces.delete failed - error message: $json->{message}\n";
+        } else {
+            carp "unexpected response from SendGrid: $response->{content}\n";
+        }
+    }
+
+    return 0;
+}
+
+#
+# _make_request
+#
+# Helper function to build the URL and then make the request to SendGrid
+# $action is the part of the endpoint that identifies the action
+#   eg for getting bounces, the base URL is https://sendgrid.com/api/bounces.get.json
+#   and $action will be 'bounces.get'
+# $optref is a hash reference that contains any options passed by the caller of the
+#   public function
+# $defaults is a hashref containing any defaults that we want to mix in,
+#   regardless of what the user passed to the public function
+#
+sub _make_request
+{
+    my $self     = shift;
+    my $action   = shift;
+    my $optref   = shift;
+    my $defaults = shift;
     my %params   = (
                     api_user => $self->api_user,
                     api_key  => $self->api_key,
-                    date     => 1,
+                    %$defaults,
                    );
+    my $url      = 'https://sendgrid.com/api/'.$action.'.json';
     my $response;
-    my $uri;
-    my $data;
-    my (@bounces, $bounce);
 
-    foreach my $opt (keys %opts) {
-        if (not exists($bounce_params{$opt})) {
-            carp "Mail::SendGrid::bounces(): unknown parameter '$opt'";
-            return ();
+    foreach my $opt (keys %$optref) {
+        if (not exists($valid_params{$action}->{$opt})) {
+            carp "Mail::SendGrid unknown parameter '$opt' for $action";
+            return 0;
         }
-        if ((not defined($opts{$opt})) || ($opts{$opt} !~ /^($bounce_params{$opt})$/)) {
-            carp "Mail::SendGrid::bounces(): invalid value '$opts{$opt}' for parameter '$opt'";
-            return ();
+        if ((not defined($optref->{$opt})) || ($optref->{$opt} !~ /^($valid_params{$action}->{$opt})$/)) {
+            carp "Mail::SendGrid invalid value '$optref->{$opt}' for parameter '$opt' to $action";
+            return 0;
         }
-        $params{$opt} = $opts{$opt};
+        $params{$opt} = $optref->{$opt};
     }
 
-    $uri      = $base_uri.'?'.join('&', map { $_.'='.uri_escape($params{$_}) } keys %params);
-    $response = HTTP::Tiny->new->get($uri);
+    $url .= '?'.join('&', map { $_.'='.uri_escape($params{$_}) } keys %params);
 
-    if ($response->{success})
-    {
-        $data = XMLin($response->{content},
-                      SuppressEmpty => '', ForceArray => [ 'bounce' ]);
-        if (ref($data) && exists($data->{'bounce'})) {
-            foreach my $hashref (@{ $data->{'bounce'} })
-            {
-                $bounce = Mail::SendGrid::Bounce->new($hashref);
-                push(@bounces, $bounce) if defined($bounce);
-            }
-        }
-    }
-    else
-    {
-        carp "bounces request failed\n",
+    $response = $self->ua->get($url);
+    if (not $response->{success}) {
+        carp __PACKAGE__, " : $action HTTP request failed\n",
              " status code = $response->{status}\n",
              " reason      = $response->{reason}\n";
     }
-    return @bounces;
+
+    return $response;
 }
 
 1;
@@ -84,16 +145,17 @@ sub bounces
  
  $sendgrid = Mail::SendGrid->new('api_user' => '...', 'api_key' => '...');
  print "Email to the following addresses bounced:\n";
- foreach my $bounce ($sendgrid->bounces)
- {
+ foreach my $bounce ($sendgrid->bounces) {
      print "\t", $bounce->email, "\n";
  }
+ 
+ $sendgrid->delete_bounces(email => 'neilb@cpan.org');
 
 =head1 DESCRIPTION
 
 This module provides easy access to the APIs provided by sendgrid.com, a service for sending emails.
-At the moment the module just provides the C<bounces()> method. Over time I'll add more of the
-SendGrid API.
+At the moment the module just provides the C<bounces()> and C<delete_bounces()> methods.
+Over time I'll add more of the SendGrid API.
 
 =method new
 
@@ -148,6 +210,33 @@ For example, to get a list of all soft bounces over the last week, you would use
 
   @bounces = $sendgrid->bounces(type => 'soft', days => 7);
 
+=method delete_bounces( %options )
+
+This is used to delete one or more bounces, or even all bounces;
+the following options constrain which bounces are deleted.
+For a description of the options, see L<"bounces">.
+
+=over 4
+
+=item *
+
+start_date
+
+=item *
+
+end_date
+
+=item *
+
+type
+
+=item *
+
+email
+
+=back
+
+
 =head1 SEE ALSO
 
 =over 4
@@ -158,6 +247,6 @@ The class which defines the data objects returned by the bounces method.
 
 =item SendGrid API documentation
 
-L<http://docs.sendgrid.com/documentation/api/web-api/webapibounces/>
+L<http://sendgrid.com/docs/API%20Reference/Web%20API/bounces.html>
 
 =back
